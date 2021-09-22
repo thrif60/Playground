9172fc087c2d78418d4ef494896c8286ed8700af
thrif60 <thrif60@gmail.com>
2021-09-23 01:27:59 +0900

    first commit

---
+++ app/src/main/java/io/github/thrif60/playground/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.github.thrif60.playground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
